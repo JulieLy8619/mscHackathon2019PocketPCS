2c97f057d2fc17a55fbee40c8410be7ab71c9681
isaac_thinkpad <isaac@thinkpad>
2019-11-08 22:28:23 -0800

    Added Moves dbset

--- pocketPCS/Controllers/YourTripController.cs
+++ pocketPCS/Controllers/YourTripController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using pcsHackathon2019.Models;
+using pocketPCS.Data;
 
 namespace pcsHackathon2019.Controllers
 {
     public class YourTripController : Controller
-    {        
+    {
+        private readonly ApplicationDbContext context;
+
+        public YourTripController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
 
         public IActionResult Index()
         {
@@ -18,6 +25,17 @@ namespace pcsHackathon2019.Controllers
         [HttpPost]
         public IActionResult Index(Move move)
         {
+            if (ModelState.IsValid)
+            {
+                context.Moves.Add(move);
+                context.SaveChanges();
+            }
+            else
+            {
+                return View();
+            }
+
+
             return RedirectToAction("Index", "Budget", move);
         }
 
--- pocketPCS/Data/ApplicationDbContext.cs
+++ pocketPCS/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using pcsHackathon2019.Models;
 
 namespace pocketPCS.Data
 {
@@ -12,5 +13,8 @@ namespace pocketPCS.Data
             : base(options)
         {
         }
+
+        public DbSet<Move> Moves { get; set; }
+       
     }
 }

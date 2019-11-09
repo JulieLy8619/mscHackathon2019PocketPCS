04ecf8f7dcbc928f15a2692696aa80d734e90006
Julie Ly <July8619@yahoo.com>
2019-11-08 23:57:55 -0800

    worked on comment controller

--- pcsHackathon2019/Controllers/CommentController.cs
+++ pcsHackathon2019/Controllers/CommentController.cs
@@ -10,10 +10,44 @@ namespace pcsHackathon2019.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //will list all the comments for that thread
+        //need to get passed the threadID
         // GET: /<controller>/
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int id)
+        {
+            //need to query to only show comments for a given threadID
+            //view needs to pass in the thread ID when this is called
+            var coms = from c in context.CommentTable
+                       select c;
+            coms = coms.Where(e => e.ThreadID == id);
+            return View(await coms.ToListAsync());
+        }
+
+        //should take us to the add comment form page
+        public IActionResult AddComment()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddComment([Bind("ID,ThreadID,Username,Subject,Body")] Comment comment)
+        {
+            Comment newComment = comment;
+            if (ModelState.IsValid)
+            {
+                await context.CommentTable.Add(newComment);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comment);
         }
     }
 }

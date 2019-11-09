b991d797b2de3392e76e753fedecbaf7db9af2f2
isaac_thinkpad <isaac@thinkpad>
2019-11-08 19:49:39 -0800

    Added YourTrip and TripResults Controllers. My thought was the YourTrip results will redirect to the TripResults view once the user fills out the form.

---
+++ pcsHackathon2019/Controllers/TripResultsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pcsHackathon2019.Controllers
+{
+    public class TripResultsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ pcsHackathon2019/Controllers/YourTripController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pcsHackathon2019.Controllers
+{
+    public class YourTripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Move move)
+        {
+            RedirectToAction("Index", "Budget", move);
+        }
+
+    }
+}

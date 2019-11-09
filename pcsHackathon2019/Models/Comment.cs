eec3f3b02fe87e9c66ae1338deb03337508b7464
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-08 21:40:20 -0800

    Models

---
+++ pcsHackathon2019/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace pcsHackathon2019.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int ThreadID { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public virtual Thread Thread { get; set; }
+    }
+}
---
+++ pcsHackathon2019/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace pcsHackathon2019.Models
+{
+    public class Expense
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public virtual Budget Budget { get; set; }
+    }
+}
